2e3f6a268e8e15dc424096dac2927918c7934f3d
okoliken <jahbuchimokoli3@gmail.com>
2023-10-31 10:39:57 +0100

    fix to side bar issue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-import { sideBarLinks } from '../utils/sidebarlinks'
-import { Icon } from '@iconify/vue'
+import { sideBarLinks } from "../utils/sidebarlinks";
+import { Icon } from "@iconify/vue";
+
+const linkText = (text: string) => {
+  console.log(text);
+};
 </script>
 
 <template>
   <div
     class="w-full bg-black md:bg-none min-h-screen fixed top-0 bottom-0 z-50 bg-opacity-20 md:z-30 md:bg-opacity-0 md:w-[240px]"
   >
-    <aside class="min-h-screen w-full max-w-[240px] fixed top-0 bottom-0 sidebar  lg:block">
+    <aside
+      class="min-h-screen w-full max-w-[240px] fixed top-0 bottom-0 sidebar lg:block"
+    >
       <div
         class="w-full bg-[#79242F] max-w-[200px] my-4 mx-4 rounded-[6px] py-[13px] px-[11px] user-details flex items-center gap-x-2"
       >
@@ -22,28 +28,56 @@ import { Icon } from '@iconify/vue'
           <li
             :key="index"
             v-for="(links, index) in sideBarLinks"
-            :class="{ 'my-12 font-bold': links.type == 'sub-headings' }"
-            class="text-justify"
+            :class="{
+              'my-12 font-bold': links.type == 'sub-headings',
+              '': linkText(links.title),
+            }"
+            class="text-justify w-full"
           >
-            <div class="flex items-center gap-x-3 mb-6">
-              <Icon v-if="links.icon" :icon="links.icon" />
+            <div
+              :class="{
+                'border-l-4 pl-3 border-[#79242F] rounded transform -translate-x-4':
+                  links.title === 'Dashboard',
+              }"
+              class="flex items-center gap-x-3 mb-6 link"
+            >
+              <Icon
+                :class="{
+                  '!text-[#79242F] font-extrabold': links.title === 'Dashboard',
+                }"
+                v-if="links.icon"
+                :icon="links.icon"
+              />
               <p
                 :class="{
-                  'font-extrabold text-[#A4A6A8] tracking-[0.1px]': links.type === 'sub-headings'
+                  'font-extrabold text-[#A4A6A8] tracking-[0.1px]':
+                    links.type === 'sub-headings',
                 }"
                 v-if="links.type == 'sub-headings'"
               >
                 {{ links.title }}
               </p>
-              <router-link v-else class="text-[14px]" to="/">{{ links.title }}</router-link>
+              <router-link
+                :class="{
+                  '!text-[#79242F] font-extrabold': links.title === 'Dashboard',
+                }"
+                v-else
+                class="text-[14px]"
+                to="/"
+                >{{ links.title }}</router-link
+              >
             </div>
             <div
-              class="flex items-center gap-x-3 mb-6"
+              class="flex items-center gap-x-3 mb-6 link"
               :key="index"
               v-for="(link, index) in links.content"
             >
               <Icon :icon="link.icon" />
-              <router-link class="text-[14px]" to="#">{{ link.title }}</router-link>
+              <router-link
+                class="text-[14px] hover:!text-[#79242F] font-medium"
+                to="#"
+                >{{ link.title }}</router-link
+              >
             </div>
           </li>
         </ul>
@@ -80,7 +114,15 @@ import { Icon } from '@iconify/vue'
   display: none; /* Safari and Chrome */
 }
 
-.sidebar svg {
+.sidebar .link:hover svg {
+  color: #79242f;
+}
+
+.sidebar .link:hover a {
+  color: #79242f !important;
+}
+
+.sidebar .link svg {
   color: #4f4f4f;
 }
 .sidebar p {
@@ -91,6 +133,4 @@ import { Icon } from '@iconify/vue'
   font-size: 12px;
   color: #ffffff;
 }
-
-
 </style>
